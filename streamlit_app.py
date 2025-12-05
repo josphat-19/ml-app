a6a038f9f8b1b8ff447f41c1ca5d81dd14ffb219
Josphat Gitahi <146937590+josphat-19@users.noreply.github.com>
2025-12-05 10:57:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ st.title('🤖 Machine Learning App')
 st.info('This app builds a machine learning model')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-  df
+	st.write('**Raw data**')
+	df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+	df
 
   st.write('**X**')
   X = df.drop('species', axis=1)
@@ -20,3 +20,11 @@ with st.expander('Data'):
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+# Data preparations
+with st.sidebar:
+  st.header('Input features')
+  # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox('Gender', ('male', 'female'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
