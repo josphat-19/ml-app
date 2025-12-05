6e18a598dc99a219d3f7ba059d05eeb7f43a30b0
Josphat Gitahi <146937590+josphat-19@users.noreply.github.com>
2025-12-05 11:07:56 +0300

    Fix DataFrame import by using pd.DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.sidebar:
 			'flipper_length_mm': flipper_length_mm,
 			'body_mass_g': body_mass_g,
 			'gender': gender}
-	input_df = df.DataFrame(data, index=[0])
+	input_df = pd.DataFrame(data, index=[0])
 	input_df
 
 
