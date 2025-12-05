3f45c13455fd420b8b1ea72b6c89047be8ce54f4
Josphat Gitahi <146937590+josphat-19@users.noreply.github.com>
2025-12-05 10:58:19 +0300

    Refactor variable assignments and formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,21 +10,20 @@ with st.expander('Data'):
 	df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 	df
 
-  st.write('**X**')
-  X = df.drop('species', axis=1)
-  X
+	st.write('**X**')
+	X = df.drop('species', axis=1)
+	X
 
-  st.write('**y**')
-  y = df.species
-  y
+	st.write('**y**')
+	y = df.species
+	y
 
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+	st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 # Data preparations
 with st.sidebar:
-  st.header('Input features')
-  # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  gender = st.selectbox('Gender', ('male', 'female'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+	st.header('Input features')
+	island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+	gender = st.selectbox('Gender', ('male', 'female'))
+	bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
