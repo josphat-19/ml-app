94ff28f5f90c6d7a238532b97fbd24b258382e49
Josphat Gitahi <146937590+josphat-19@users.noreply.github.com>
2025-12-05 10:47:42 +0300

    Fix scatter chart x-axis unit from nm to mm

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ with st.expander('Data'):
   y
 
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='bill_length_nm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
